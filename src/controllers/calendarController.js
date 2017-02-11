e6182835bca689eb71199563f60cfa0e78ebb3a9
Wenliang-CHEN <wenliang284713@gmail.com>
2017-02-11 22:51:46 +0800

    #modal-backdrop-issue-fixing

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -82,10 +82,10 @@ CalendarApp.controller("CalendarContorller", ['toastr','$scope', '$http', '$docu
 				scope: $scope
 			}).then(function(modal) {
 				
-				modal.element.modal({backdrop: 'static'});
+				modal.element.modal();
 				
 				modal.close.then(function(dailyEventObj) {
-					angular.element($document[0].getElementsByClassName('modal-backdrop')).remove();
+					//angular.element($document[0].getElementsByClassName('modal-backdrop')).remove();
 				});
 			});
 		} else {
@@ -110,48 +110,4 @@ CalendarApp.controller("CalendarContorller", ['toastr','$scope', '$http', '$docu
 	}
 }]);
 
-CalendarApp.controller('ModalController', function($scope, dailyEventObj, close, toastr, yearMonthKey) {
-	$scope.dailyEventObj = dailyEventObj;
-	$scope.currentEvents = [];
 
-	//For Current Event, we must push the elements one by one again so that the array does not pass by reference
-	angular.forEach(dailyEventObj.events, function(event){
-		var newEvent = new Event();
-		newEvent.processJSON(event);
-		$scope.currentEvents.push(newEvent);
-	})
-
-	$scope.close = function() {
-		close(dailyEventObj);
-	};
-
-	$scope.addNewEvent = function(){
-		$scope.currentEvents.push(new Event());
-	}
-
-	$scope.updatedailyEventObj = function(){
-		var allEventsValid = true;
-
-		angular.forEach($scope.currentEvents, function(event){
-			if(!event.validate()){
-				allEventsValid = false;
-			}
-		});
-
-		if(allEventsValid){
-			dailyEventObj.events = $scope.currentEvents;
-
-			//save to localStorage
-			localStorage.setItem(yearMonthKey, JSON.stringify($scope.events));
-
-			toastr.success('Events updated!');
-			close(dailyEventObj);
-		}
-	}
-
-	$scope.removeEvent = function(index){
-		$scope.currentEvents.splice(index, 1);
-	}
-
-	//TODO: toast, after save, modal scroll
-});
