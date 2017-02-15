f90b6c92f174693bcdcc7052a3e2079dd5c44faa
wenliang <wenliang284713@gmail.com>
2017-02-15 23:19:29 +0800

    #TODO": Totally switched to bootstrap modal, need to debug

--- src/controllers/calendarController.js
+++ src/controllers/calendarController.js
@@ -1,111 +1,119 @@
 
-CalendarApp.controller("CalendarContorller", ['toastr','$scope', '$http', '$document', 'DataService', 'CalendarService', 'ModalService',
-	function (toastr, $scope, $http, $document, DataService, CalendarService, ModalService) {
+CalendarApp.controller("CalendarContorller", ['$uibModal','toastr','$scope', '$http', '$document', 'DataService', 'CalendarService',
+	function ($uibModal, toastr, $scope, $http, $document, DataService, CalendarService) {
 	//Init
-	$scope.yearList = [];
-	$scope.monthList = [];
-	$scope.events = [];
+	var $ctrl = this;
+	$ctrl.animationsEnabled = true;
+
+	$ctrl.yearList = [];
+	$ctrl.monthList = [];
+	$ctrl.events = [];
 	
-	$scope.runningDateTimestamp = 0;
-	$scope.calendarRows = 0;
-	$scope.firstDayOfWeekForEvents = 0;
-	$scope.calendarColumns = CalendarService.getColumns();
-	$scope.calendarStarted = false;
+	$ctrl.runningDateTimestamp = 0;
+	$ctrl.calendarRows = 0;
+	$ctrl.firstDayOfWeekForEvents = 0;
+	$ctrl.calendarColumns = CalendarService.getColumns();
+	$ctrl.calendarStarted = false;
 	
 	var yearMonthKey = '';
 
 	var date = new Date();
-	$scope.year = date.getFullYear().toString();
-	$scope.month = CalendarService.formatMonthToString(date.getMonth());
+	$ctrl.year = date.getFullYear().toString();
+	$ctrl.month = CalendarService.formatMonthToString(date.getMonth());
 
 	DataService.getYearMonthList().success(function(data){
-		$scope.yearList = data.years;
-		$scope.monthList = data.months;
+		$ctrl.yearList = data.years;
+		$ctrl.monthList = data.months;
 	});
 	
-	$scope.refreshCalendar = function(){
-		yearMonthKey = $scope.year + '_' + $scope.month;
-		$scope.calendarStarted = false;
+	$ctrl.refreshCalendar = function(){
+		yearMonthKey = $ctrl.year + '_' + $ctrl.month;
+		$ctrl.calendarStarted = false;
 	
-		var savedEvents = JSON.parse(localStorage.getItem(yearMonthKey));
+		var savedEvents =localStorage.getItem(yearMonthKey);
 
-		if(isEmpty(savedEvents)){
+//TODO: now the events are blank
+		//if(isEmpty(savedEvents)){
 			DataService.getEventList().success(function(data){
 				//TODO: to loop the events one more time seem not so effective, let see what i could do
-				$scope.events = CalendarService.reorganizeEvent($scope.year, $scope.month, savedEvents);
-				localStorage.setItem(yearMonthKey, JSON.stringify($scope.events));
-			});
-		} else {
-			$scope.events =  CalendarService.processEventJSON(savedEvents); 
-		}
-	};
-	$scope.refreshCalendar();
+				$ctrl.events = CalendarService.reorganizeEvent($ctrl.year, $ctrl.month, data);
 
-	$scope.isValidDay = function(dayOfWeek, timestamp){
+				console.log(data);
+				localStorage.setItem(yearMonthKey, JSON.stringify($ctrl.events));
+			});
+		//} else {
+	//		savedEvents = JSON.parse(savedEvents);
+	//		$ctrl.events =  CalendarService.processEventJSON(savedEvents); 
+	//	}
+	};
+	$ctrl.refreshCalendar();
+
+	$ctrl.isValidDay = function(dayOfWeek, timestamp){
 		//if calendar started, valid
-		if($scope.calendarStarted){
-			return typeof ($scope.events[timestamp] != 'undefined');
+		if($ctrl.calendarStarted){
+			return typeof ($ctrl.events[timestamp] != 'undefined');
 		}
 		//else, only the first day of week of the running timestamp will trigger the start
 		if(new Date(parseInt(timestamp)).getDay() == dayOfWeek){
-			$scope.calendarStarted = true;
+			$ctrl.calendarStarted = true;
 			
-			return (typeof $scope.events[timestamp] != 'undefined');
+			return (typeof $ctrl.events[timestamp] != 'undefined');
 		} else {
 			return false;
 		}
 	}
 	
-	$scope.printDay = function(timestamp){
+	$ctrl.printDay = function(timestamp){
 		return CalendarService.formatToTwoDigits(new Date(parseInt(timestamp)).getDate());
 	}
 	
-	$scope.onParamChange = function(){
-		$scope.refreshCalendar();
+	$ctrl.onParamChange = function(){
+		$ctrl.refreshCalendar();
 	}
 	
-	$scope.getMonthDisplay = function(month){
+	$ctrl.getMonthDisplay = function(month){
 		return CalendarService.getShortMonthText(month);
 	}
 
 
-	$scope.showDetail = function(dailyEventObj) {
-		if(dailyEventObj.valid){
-			ModalService.showModal({
-				templateUrl: 'calenderDetailModal.html',
-				controller: "ModalController",
-				inputs: {
-					dailyEventObj: dailyEventObj,
-					toastr: toastr,
-					yearMonthKey: yearMonthKey
-				},
-				scope: $scope
-			}).then(function(modal) {
-				
-				modal.element.modal();
-				
-				modal.close.then(function(dailyEventObj) {
-					//angular.element($document[0].getElementsByClassName('modal-backdrop')).remove();
-				});
-			});
-		} else {
-			toastr.warning('Invalid date.');
-		}
-    };
 
+  $ctrl.showDetail = function (dailyEventObj) {
+
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'calenderDetailModal.html',
+      controller: 'ModalController',
+      controllerAs: '$ctrl',
+	  size: 'lg',
+      resolve: {
+        dailyEventObj: function () {
+          return dailyEventObj;
+        },
+		yearMonthKey: function () {
+          return yearMonthKey;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (returnedDailyEventObj) {
+    }, function () {
+      toastr.success('Events updated!');
+    });
+  };
 	
-	$scope.formatDate = function(timestamp){
+	$ctrl.formatDate = function(timestamp){
 		return CalendarService.formatDate(timestamp);
 	}
 
 	//expose the function outside
-	$scope.isEmpty = function(obj){
+	$ctrl.isEmpty = function(obj){
 		return isEmpty(obj)
 	}
 
 	//this is a very strict function to determine if an obj is a "empty" object
 	function isEmpty(obj){
-		return (typeof obj == 'undefined' || obj == null
+		return (typeof obj == 'undefined' || obj == 'undefined' || obj == null
 			|| JSON.stringify(obj) === JSON.stringify({}) || obj.length <= 0);
 	}
 }]);
